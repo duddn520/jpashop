72a893813758880c890b4cc9ac0f6207d6bdea0f
duddn520 <duddn520@konkuk.ac.kr>
2021-08-18 20:16:35 +0900

    엔티티 생성&매핑, 회원 레포지토리와 서비스 개발 및 테스트 작성

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<Orderitem> orderitems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(Orderitem orderitem){
+        orderitems.add(orderitem);
+        orderitem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
